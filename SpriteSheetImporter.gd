e30d5d66bda9bcc4cffd2f14ef165596240a5c88
= <lucasbenninger@protonmail.com>
2018-07-14 05:10:59 -0400

    Add Script

---
+++ SpriteSheetImporter.gd
@@ -0,0 +1,37 @@
+tool
+extends Node
+
+export (bool) var Generate = false setget onGenerate
+
+export var tileSize = 16
+export var tileCountX = 0
+export var tileCountY = 0
+export var offset = 1
+export (Texture) var spritesheet
+
+
+func _ready():
+    pass
+
+func onGenerate(isTriggered):
+	if(isTriggered):
+		Generate = false
+		var yPoint = 0
+		for y in range(tileCountY):
+			var xPoint = 0
+			for x in range(tileCountX):
+				var positionVect = Vector2(xPoint,yPoint)
+				var sizeVect = Vector2(tileSize, tileSize)
+				var tileRegion = Rect2(positionVect, sizeVect)
+				
+				var tile = Sprite.new()
+				add_child(tile)
+				tile.set_owner(self)
+				tile.set_name(str(x)+"-"+str(y))
+				tile.set_texture(spritesheet)
+				tile.set_region(true)
+				tile.set_region_rect(tileRegion)
+				tile.position = Vector2(x*tileSize+tileSize/2, y*tileSize+tileSize/2)
+				
+				xPoint += tileSize + offset
+			yPoint += tileSize + offset
